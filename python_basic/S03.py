a4ed25fd48098d42ea99175da4ec7c87d7fae3a4
zahrabakhshandeh <zahra.bakhshandeh1@gmail.com>
2023-11-03 15:41:11 +0330

    added S03

---
+++ python_basic/S03.py
@@ -0,0 +1,52 @@
+# change to str()
+num1 = 12
+num2 = 15.6
+flag = True
+"""print(str(num1))
+print(str(num2))
+print(str(flag))"""
+# change to bool()
+# falsy ====> 0, 0.0, "", False
+num1 = "0"
+#print(bool(int(num1)))
+# Points
+num1 = 0
+num2 = 14
+#print(num1 or num2)
+user = "blue"
+default = "balck"
+#print(user or default)
+num1 = 12
+num2 = 40
+res = num1 + num2
+# print(res)
+#num1 = float(input("Enter your number: "))
+#print(num1 % 2 == 0)
+score = float(input("math score: ")) # 12
+#print(score >= 10)
+# Condiions
+if score < 0 or score > 20:
+    print("out of range")
+if score >= 10 and score <= 20: # if True:
+    print("yes") # run
+if score < 10 and score > 0: # if False
+    print("no")
+print("end")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
