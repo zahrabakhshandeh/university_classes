3a0083cdd1868345175fcd812ee83bebf914b376
zahrabakhshandeh <zahra.bakhshandeh1@gmail.com>
2023-12-04 11:24:24 +0330

    S11

--- python_advanced/S11_part2.py
+++ python_advanced/S11_part2.py
@@ -1,14 +1,23 @@
 # stu_num, name, age, scores
+# students = {
+#      "123" : {
+	
+#		}	
+#}
 # limit ===> stu_num
 # add, display, edit, remove, search, details
+def change_to_list(scores):
+	scores = scores.split(",")
+	scores = list(map(float, scores))
+	return scores
+
 def add():
 	st_num = input("student number: ")
 	if st_num not in students:
 		name = input("student name: ")
 		age = int(input("age: "))
 		scores = input("scores: ")
-		scores = scores.split(",")
-		scores = list(map(float, scores))
+		scores = change_to_list(scores)
 		student = {
 		"name" : name,
 		"age" : age,
@@ -28,17 +37,45 @@ def display():
 			print(f"{i}: {v}")
 
 
-def edit():
-	pass
+def edit(stu_num):
+	if stu_num in students:
+		new_name = input("student name: ")
+		new_age = input("age: ")
+		new_scores = input("scores: ")
+		students[stu_num]["name"] = new_name or students[stu_num]["name"]
+		students[stu_num]["age"] = int(new_age or students[stu_num]["age"])
+		if new_scores != "":
+			new_scores = change_to_list(new_scores)
+			students[stu_num]["scores"] = new_scores
+	else:
+		print(f"{stu_num}: not found!")
 
-def remove():
-	pass
+def remove(stu_num):
+	if stu_num in students:
+		students.pop(stu_num)
+		print("removed!")
+	else:
+		print(f"{stu_num}: not found!")
 
-def search():
-	pass
+def search(start, end):
+	for stu in students.values():
+		for s in stu["scores"]:
+			if start < s < end:
+				for i, j in stu.items():
+					print(f"{i} : {j}")
+				break
 
 def details():
-	pass
+	numbers_of_stu = len(students)
+	print(f"number of students: {numbers_of_stu}")
+	scores = []
+	for stu in students.values():
+		scores += stu["scores"]
+	sum_scores = sum(scores)
+	max_scores = max(scores)
+	min_scores = min(scores)
+	avg_scores = sum_scores / len(scores)
+	print(sum_scores, max_scores, min_scores, avg_scores)
 
 # ...........main..........
 students = {}
@@ -47,13 +84,19 @@ for i in range(100):
 	if cmd == "add":
 		add()
 	elif cmd == "remove":
-		remove()
+		stu_num = input("student number to remove: ")
+		remove(stu_num)
 	elif cmd == "display":
 		display()
 	elif cmd == "edit":
-		edit()
+		stu_num = input("student number to edit: ")
+		edit(stu_num)
 	elif cmd == "search":
-		search()
+		start = input("start point: ")
+		end = input("end point: ")
+		start = float(start or 0)
+		end = float(end or 20)
+		search(start, end)
 	elif cmd == "detail":
 		details()
 	elif cmd =="exit":
